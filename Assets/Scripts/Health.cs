ff69c076d6732e5f58b74dbc4ac91f3246a17b25
artnovik <a.novikov.dev@gmail.com>
2018-02-02 12:42:45 +0200

    Health sync

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
+    public RectTransform healthBar;
+
     public const int MaxHealth = 100;
 
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = MaxHealth;
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+        {
+            return;
+        }
+
         currentHealth -= amount;
 
         if (currentHealth <= 0)
@@ -17,5 +27,11 @@ public class Health : MonoBehaviour
             currentHealth = 0;
             Debug.Log(gameObject.name + " is dead");
         }
+    }
+
+    private void OnChangeHealth(int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+
     }
 }
