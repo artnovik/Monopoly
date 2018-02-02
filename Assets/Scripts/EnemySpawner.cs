4762c1dd54d9431d5a61796007b3bfa860b40dff
artnovik <a.novikov.dev@gmail.com>
2018-02-02 15:28:54 +0200

    Spawning and Respawning

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-namespace Network
+namespace NetworkShooter
 {
     public class EnemySpawner : NetworkBehaviour
     {
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -8,6 +8,7 @@ namespace NetworkShooter
 {
     public class Health : NetworkBehaviour
     {
+        private NetworkStartPosition[] spawnPoints;
         public RectTransform healthBar;
 
         public const int MaxHealth = 100;
@@ -16,6 +17,14 @@ namespace NetworkShooter
 
         [SyncVar(hook = "OnChangeHealth")]
         public int currentHealth = MaxHealth;
+
+        private void Start()
+        {
+            if (isLocalPlayer)
+            {
+                spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+            }
+        }
 
         public void TakeDamage(int amount)
         {
@@ -55,7 +64,19 @@ namespace NetworkShooter
             if (isLocalPlayer)
             {
                 // Move back to zero location
-                transform.position = Vector3.zero;
+                //transform.position = Vector3.zero;
+
+                // Set the spawn point to origin as a default value
+                Vector3 spawnPoint = Vector3.zero;
+
+                // If there is a spawn point array and the array is not empty, pick a spawn point at random
+                if (spawnPoints != null && spawnPoints.Length > 0)
+                {
+                    spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+                }
+
+                // Set the player's position to the chosen spawn point
+                transform.position = spawnPoint;
             }
         }
     }
