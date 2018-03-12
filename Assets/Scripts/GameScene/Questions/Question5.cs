14e1163e0e4c06cb787b5da278692bc0ea6e863e
artnovik <a.novikov.dev@gmail.com>
2018-03-12 18:35:05 +0200

    GameScene hierarchy improvement | Question5

---
+++ Assets/Scripts/GameScene/Questions/Question5.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Question5 : QuestionData
+{
+    private bool answerRight;
+
+    private uint scoreValue;
+
+    [SerializeField]
+    private GameObject rightAnswerButton;
+
+    public void ButtonAnswerSelect(GameObject buttonClicked)
+    {
+        answerRight = buttonClicked == rightAnswerButton;
+
+        Debug.Log("This (" + int.Parse(buttonClicked.name.Replace("Answer", string.Empty)) + ") answer is " + answerRight);
+    }
+
+    public void ButtonConfirmAnswer()
+    {
+        if (answerRight)
+        {
+            scoreValue = scoreMaxValue;
+            gameManager.playerData.AddPlayerScore(scoreValue);
+        }
+
+        foreach (var button in buttonsConfirmAnswer)
+        {
+            button.GetComponent<Button>().interactable = false;
+        }
+
+        gameManager.answerDone = true;
+
+        gameObject.SetActive(false);
+        gameManager.ResetTimer(false);
+    }
+}
